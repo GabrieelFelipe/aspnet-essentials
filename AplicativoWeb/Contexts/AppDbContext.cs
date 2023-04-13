49b805e3876955310a85bf4e4c6be85998b113b5
GabrieelFelipe <gabrielfelipe1014@gmail.com>
2023-04-13 16:07:56 -0300

    aula 4

--- AplicativoWeb/Contexts/AppDbContext.cs
+++ AplicativoWeb/Contexts/AppDbContext.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
 
 //Trabalho para Amanhã:  Instalar Dependências
-public class AppDbContext{
+public class AppDbContext:DbContext{
 
+//override (subrescrever)
+//NãoTrair( ) - Contrato
+//CompartilhamentoCarteira
+//protected
 
+protected override void OnConfiguring(DbContextOptionsBuilder builder){
+ builder.UseSqlServer("Server=LA-F08-17;DataBase=EscolaDotNet;User Id=sa;Password=senai@123;TrustServerCertificate=True");
 
+}
 }
 
 
